0c8a7c2057f60572dc938fb1bf3b2c34915e8ef6
Victor Vilela <vilela_victor@hotmail.com>
2017-06-15 00:10:22 -0500

    pregunta 1 : llave, parentesis y corchetes balanceados.

---
+++ Questions.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Questions.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Questions.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("405f8b52-b3e0-4d04-af3a-5eb4158121bb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Questions/01.Balanced.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions
+{
+    public class _01_Balanced
+    {
+		/*
+		You're working with an intern that keeps coming to you with JavaScript code that won't run because the braces, brackets, and parentheses are off. To save you both some time, you decide to write a braces/brackets/parentheses validator. 
+		Let's say: 
+		'(', '{', '[' are called "openers." 
+		')', '}', ']' are called "closers." 
+		Write an efficient function that tells us whether or not an input string's openers and closers are properly nested. 
+		Examples: 
+		"{ [ ] ( ) }" should return true 
+		"{ [ ( ] ) }" should return false 
+		"{ [ }" should return false 
+		*/
+
+        private Dictionary<string, string> closers = new Dictionary<string, string>() {
+                { "{", "}" },
+                { "(", ")" },
+                { "[", "]" }
+            };
+
+        public bool EstaBalanceado(string cadena)
+        {
+
+
+            if (string.IsNullOrWhiteSpace(cadena))
+            {
+                return false;
+            }
+
+            if (cadena.Length % 2 != 0)
+            {
+                return false;
+            }
+
+            if (!(cadena[0].ToString() == "{" && cadena[cadena.Length - 1].ToString() == "}"))
+            {
+                return false;
+            }
+
+            for (int i = 1; i < cadena.Length - 1; i = i + 2)
+            {
+                var c1 = cadena[i].ToString();
+                var c2 = cadena[i + 1].ToString();
+
+                if (c2 != closers[c1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
